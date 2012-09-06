600489f6dc58b43b6c0c98427aa7e4ab159a4270
IrynaAndryianets <ivandryi@unca.edu>
2012-09-05 20:34:00 -0400

    modofied hello to find the sum of all the multiples of 3 and 5 below 1000

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,17 @@
+/* Find the sum of all the multiples of 3 and 5 below 1000 (not inclusive) */
+
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
 int main(){
-  cout << "Hello, World!!!" << endl;
+  int sum = 0;
+  for (int i = 1; i < 1000; ++i){
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum = sum + i;
+    }
+  }
+  cout << "The sum is " << sum << endl;
   return 0;
 }
